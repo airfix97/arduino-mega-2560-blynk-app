c2d309b19ce328118039ddbf1ab2d3f73e26bb79
Ankit Khandeparkar <anky199714@gmail.com>
2020-03-10 17:13:05 +0530

    Add files via upload

---
+++ led_blynk_mega_test_2560.ino
@@ -0,0 +1,129 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  WARNING!
+    It's very tricky to get it working. Please read this article:
+    http://help.blynk.cc/hardware-and-libraries/arduino/esp8266-with-at-firmware
+
+  This example shows how value can be pushed from Arduino to
+  the Blynk App.
+
+  NOTE:
+  BlynkTimer provides SimpleTimer functionality:
+    http://playground.arduino.cc/Code/SimpleTimer
+
+  App project setup:
+    Value Display widget attached to Virtual Pin V5
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+#include<RMCS2303drive.h>
+
+RMCS2303 rmcs;                      //object for class RMCS2303
+
+byte slave_id=7;                    //Choose the slave id of connected drive.
+int INP_CONTROL_MODE=513;           //IMPORTANT: refer datasheet and set value(integer) according to application 
+int PP_gain=32;
+int PI_gain=16;
+int VF_gain=32;
+int LPR=334;
+int acceleration=5000;
+int speed=8000;
+
+long int Current_position;
+long int Current_Speed;
+
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "t3hlJbngmipPfW7Ue4LkW5CDVz5hnMmw";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "dotaonly";
+char pass[] = "dotaonly";
+
+// Hardware Serial on Mega, Leonardo, Micro...
+#define EspSerial Serial1
+
+// or Software Serial on Uno, Nano...
+//#include <SoftwareSerial.h>
+//SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+
+ESP8266 wifi(&EspSerial);
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, millis() / 1000);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  Blynk.begin(auth, wifi, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, wifi, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, wifi, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  rmcs.Serial_selection(0);       //Serial port selection:0-Hardware serial,1-Software serial
+   rmcs.Serial0(9600);             //set baudrate for usb serial to monitor data on serial monitor
+   Serial.println("RMCS-2303 Position control mode demo\r\n\r\n");
+
+   rmcs.begin(&Serial2,9600);    //Uncomment if using hardware serial port for mega2560:Serial1,Serial2,Serial3 and set baudrate. Comment this line if Software serial port is in use
+   //rmcs.begin(&myserial,9600);     //Uncomment if using software serial port. Comment this line if using hardware serial.
+   //rmcs.WRITE_PARAMETER(slave_id,INP_CONTROL_MODE,PP_gain,PI_gain,VF_gain,LPR,acceleration,speed);    //Uncomment to write parameters to drive. Comment to ignore.
+   rmcs.READ_PARAMETER(slave_id);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
+BLYNK_WRITE(V1){
+  rmcs.Absolute_position(slave_id,-50000);
+}
+BLYNK_WRITE(V2){
+  rmcs.Disable_Position_Mode(slave_id); 
+}
+BLYNK_WRITE(V3){
+  rmcs.Absolute_position(slave_id,50000); 
+}
